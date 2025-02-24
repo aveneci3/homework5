e52099bbb6e8a09a7c0e97fc0de5c897b4ae400f
Adam Venecia <aveneci3@stu.southtexascollege.edu>
2025-02-23 23:50:27 -0600

    updated nav bar

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,18 +1,22 @@
 <template>
-  <nav class="bg-slate-900 h-20 items-center text-white">
-    <RouterLink :to="{name: 'Home'}">Home</RouterLink>
-    <RouterLink :to="{name: 'Other'}">Other</RouterLink>
-    <RouterLink :to="{name: 'LoginPage'}">Login</RouterLink>
-    <RouterLink :to="{name: 'SettingsPage'}">Settings</RouterLink>
+  <nav class="bg-slate-900 h-20 flex items-center justify-center text-white">
+    <div class="flex space-x-4">
+      <RouterLink :to="{name: 'Home'}">Home</RouterLink>
+      <RouterLink :to="{name: 'Other'}">Other</RouterLink>
+      <RouterLink :to="{name: 'SettingsPage'}">Settings</RouterLink>
+
+      <div v-if="isAuthenticated">
+        Welcome {{ user.email }}
+        <button @click="logout" class="rounded-md bg-red-500 px-4 py-2 mx-2 text-red-100 hover:bg-red-700">LOGOUT</button>
+      </div>
+      <div v-else>
+        <RouterLink :to="{name: 'LoginPage'}">Login</RouterLink>
+      </div>
+    </div>
   </nav>
 </template>
 
-<style lang="postcss" scoped>
-nav {
-  @apply flex justify-center space-x-4;
-
-  & .router-link-active {
-    @apply underline underline-offset-4;
-  }
-}
-</style>
+<script setup>
+import { useAuth } from '../composables/useAuth'
+const { isAuthenticated, logout, user} = useAuth()
+</script>
